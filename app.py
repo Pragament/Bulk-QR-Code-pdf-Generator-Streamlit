967a48d816116625b008061125b2acd64e64edc7
Nikhil Dubbaka <nikhildubbaka@gmail.com>
2023-09-28 15:04:40 +0530

    QR code size

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
 import streamlit as st
 import qrcode
 from qrcode.image.pil import PilImage
-from PIL import Image
 import io
-import base64
 from reportlab.lib.pagesizes import letter
 from reportlab.platypus import SimpleDocTemplate, Image as PlatypusImage, PageBreak, Paragraph
-from reportlab.platypus import Spacer
 from reportlab.lib.styles import getSampleStyleSheet
-from reportlab.platypus import PageTemplate, Frame
-from reportlab.lib.units import inch
+from reportlab.platypus.doctemplate import PageTemplate, BaseDocTemplate, Spacer
+from reportlab.lib.units import inch, cm
+from reportlab.platypus.frames import Frame  # Add this import statement
 
 # Function to generate QR code from text
-def generate_qr_code_with_text(text, font_size):
+def generate_qr_code_with_text(text):
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_H,
@@ -49,6 +47,9 @@ content = st.text_area(input_label, height=150)
 # Font size for text in PDF
 font_size = st.slider("Select font size for text in PDF", min_value=8, max_value=24, value=12)
 
+# QR code size
+qr_size = st.slider("Select QR code size", min_value=100, max_value=400, value=200)
+
 # Checkbox to show/hide text in PDF
 show_text = st.checkbox("Show text above QR code")
 
@@ -58,31 +59,37 @@ if st.button("Generate QR Codes and Export to PDF"):
 
         # Create a PDF document
         pdf_file_name = "qrcodes.pdf"
-        doc = SimpleDocTemplate(pdf_file_name, pagesize=letter)
+
+        class MyDocTemplate(BaseDocTemplate):
+            def __init__(self, filename, **kw):
+                self.allowSplitting = 0
+                BaseDocTemplate.__init__(self, filename, **kw)
+
+        doc = MyDocTemplate(pdf_file_name, pagesize=letter)
 
         styles = getSampleStyleSheet()
         text_style = styles["Normal"]
         text_style.fontSize = font_size
 
-        # Define a PageTemplate for adding text at the top left
-        def header(canvas, doc):
-            canvas.saveState()
-            canvas.setFont('Times-Bold', 12)
-            canvas.drawString(inch, doc.height - 0.75 * inch, "Input Text:")
-            canvas.restoreState()
+        # Define a PageTemplate for adding text and QR code
+        class MyPageTemplate(PageTemplate):
+            def __init__(self, id):
+                frames = [
+                    Frame(doc.leftMargin, doc.bottomMargin, doc.width, doc.height, id='frame', showBoundary=0),
+                ]
+                PageTemplate.__init__(self, id, frames=frames, onPage=self.header)
 
-        page_template = PageTemplate(id='page_template', frames=[
-            Frame(doc.leftMargin, doc.bottomMargin, doc.width, doc.height, id='frame', showBoundary=0),
-        ], onPage=header)
+            def header(self, canvas, doc):
+                pass  # Remove the header
 
-        doc.addPageTemplates([page_template])
+        doc.addPageTemplates([MyPageTemplate('page_template')])
         qr_code_images = []
 
         for i, c in enumerate(contents):
             c = c.strip()
             if c:
                 # Generate QR code for the line of text or URL
-                qr_code_img = generate_qr_code_with_text(c, font_size)
+                qr_code_img = generate_qr_code_with_text(c)
 
                 if show_text:
                     # Create a Paragraph with the text to display
@@ -90,14 +97,16 @@ if st.button("Generate QR Codes and Export to PDF"):
 
                 # Convert image bytes to a stream
                 img_stream = io.BytesIO(qr_code_img)
-                img = PlatypusImage(img_stream, width=doc.width, height=doc.height - 100)
+                img = PlatypusImage(img_stream, width=qr_size, height=qr_size)
 
                 if show_text:
+                    qr_code_images.append(Spacer(1, 1*cm))  # Add 1cm gap
                     qr_code_images.append(text_paragraph)
+                qr_code_images.append(Spacer(1, 1*cm))  # Add 1cm gap
                 qr_code_images.append(img)
                 qr_code_images.append(PageBreak())
 
-        # Build the PDF with one QR code and text per page
+        # Build the PDF with one QR code, text, and gap per page
         doc.build(qr_code_images)
 
         # Provide a download link for the generated PDF
@@ -105,3 +114,4 @@ if st.button("Generate QR Codes and Export to PDF"):
         st.markdown(f'<a href="{pdf_file_name}" download="{pdf_file_name}">Download QR Codes PDF</a>', unsafe_allow_html=True)
     else:
         st.error("Please enter content for the QR codes.")
+
